74fa754557d2477ebbfaddf09ae13fcf575fefe5
marco76tv <marco.sottana@gmail.com>
2021-12-07 13:01:01 +0100

    .

--- Models/Home.php
+++ Models/Home.php
@@ -8,16 +8,37 @@ namespace Modules\Amazon\Models;
 //------services---------
 //--- TRAITS ---
 use Modules\Xot\Models\Traits\WidgetTrait;
+use Sushi\Sushi;
 
 class Home extends BaseModelLang {
     use WidgetTrait;
     //use RatingTrait; // non si vota la home
+    use Sushi;
     /**
      * @var string[]
      */
     protected $fillable = ['id', 'article_type', 'icon_src'];
+    /*
+    protected $rows = [
+        [
+            'id' => 'home',
+            'name' => 'New York',
+        ],
+    ];
+    */
 
     //--------- relationship ---------------
 
     //---------- mututars -----------
+    //---------- functions ----------
+    /**
+     * GetRows.
+     */
+    public function getRows() {
+        return [
+            [
+                'id' => 1,
+            ],
+        ];
+    }
 }//end model
